782b61ad98d99f697e209bc22cc9cb8a6975ec46
Koviniho <kristian.kovac@gmx.ch>
2024-07-05 07:10:56 +0200

    Commit Abdullah Full

--- components/accets/CheckBoX.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import {Checkbox} from "@nextui-org/react";
-
-export default function CheckBoX({label}) {
-  return (
-    <Checkbox defaultSelected><p className="font-semibold">{label}</p></Checkbox>
-  );
-}
--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ["images.unsplash.com", "cdn.pixabay.com", "images.pexel.com"],
-}
-}
-
-export default nextConfig;
